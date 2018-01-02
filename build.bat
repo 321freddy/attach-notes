b1005dd85f29adf1dfb911ae3399b058f7ad78f9
321freddy <fred.ru.7@gmail.com>
2018-01-02 16:30:30 +0100

    Fixed build zipping used windows backward slashes as path seperator (zips now work on mod portal)

--- build.bat
+++ build.bat
@@ -10,7 +10,17 @@ del /f /s /q "%cd%\.build\%CurrDirName%.zip"
 
 xcopy /e /s /y "%cd%" "%temp%\.build\%CurrDirName%\" /exclude:exclude.txt
 
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%temp%\.build\', '%cd%\.build\%CurrDirName%.zip'); }"
+echo Set objArgs = WScript.Arguments > _zipIt.vbs
+echo InputFolder = objArgs(0) >> _zipIt.vbs
+echo ZipFile = objArgs(1) >> _zipIt.vbs
+echo CreateObject("Scripting.FileSystemObject").CreateTextFile(ZipFile, True).Write "PK" ^& Chr(5) ^& Chr(6) ^& String(18, vbNullChar) >> _zipIt.vbs
+echo Set objShell = CreateObject("Shell.Application") >> _zipIt.vbs
+echo Set source = objShell.NameSpace(InputFolder).Items >> _zipIt.vbs
+echo objShell.NameSpace(ZipFile).CopyHere(source) >> _zipIt.vbs
+echo wScript.Sleep 2000 >> _zipIt.vbs
 
-echo "Success!"
+CScript _zipIt.vbs "%temp%\.build\" "%cd%\.build\%CurrDirName%.zip"
+del "_zipIt.vbs"
+
+echo Success!
 call explorer "%cd%\.build\"
