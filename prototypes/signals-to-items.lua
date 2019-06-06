e236bd9f4d92a8fcfc69b621645998f6c0205249
321freddy <fred.ru.7@gmail.com>
2019-06-06 11:02:05 +0200

    fixed item duplication

--- prototypes/signals-to-items.lua
+++ prototypes/signals-to-items.lua
@@ -1,4 +1,5 @@
 local util = require("scripts.util")
+local raw = data.raw
 
 local function convertToItem(signal, type)
 	local item = util.deepCopy(signal)
@@ -15,10 +16,37 @@ local function convertToItem(signal, type)
 	return item
 end
 
-for _,signal in pairs(data.raw["virtual-signal"]) do
-	if not signal.special_signal and not data.raw.item[signal.name] then data:extend{ convertToItem(signal, "virtual-signal") } end
+local function itemExists(name)
+	return raw.item[name] or 
+		   raw.ammo[name] or 
+		   raw.armor[name] or 
+		   raw.capsule[name] or 
+		   raw.gun[name] or 
+		   raw.module[name] or 
+		   raw.tool[name] or 
+		   raw.blueprint[name] or 
+		   raw["repair-tool"][name] or 
+		   raw["rail-planner"][name] or 
+		   raw["item-with-entity-data"][name] or 
+		   raw["item-with-tags"][name] or 
+		   raw["item-with-label"][name] or 
+		   raw["item-with-inventory"][name] or 
+		   raw["copy-paste-tool"][name] or 
+		   raw["upgrade-item"][name] or 
+		   raw["deconstruction-item"][name] or 
+		   raw["blueprint-book"][name] or 
+		   raw["selection-tool"][name] or 
+		   (raw["mining-tool"] and raw["mining-tool"][name])
 end
 
-for _,fluid in pairs(data.raw["fluid"]) do
-	if not data.raw.item[fluid.name] then data:extend{ convertToItem(fluid, "fluid") } end
+for _,signal in pairs(raw["virtual-signal"]) do
+	if not signal.special_signal and not itemExists(signal.name) then 
+		data:extend{ convertToItem(signal, "virtual-signal") }
+	end
+end
+
+for _,fluid in pairs(raw["fluid"]) do
+	if not itemExists(fluid.name) then 
+		data:extend{ convertToItem(fluid, "fluid") } 
+	end
 end
