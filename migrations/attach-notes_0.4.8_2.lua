fbdceaeb3508bfd4a0733efe3d15012fea96ac93
321freddy <fred.ru.7@gmail.com>
2020-10-30 21:38:34 +0100

    Added restore signpost displays migration

---
+++ migrations/attach-notes_0.4.8_2.lua
@@ -0,0 +1,20 @@
+
+local components = scripts.components
+local metatables = require("scripts.metatables")
+metatables.refresh(global)
+
+for __,surface in pairs(game.surfaces) do
+	for __,entity in pairs(surface.find_entities_filtered{ name = "signpost" }) do
+
+		local note = global.notes[entity]
+		if note then
+			components.display.update(entity, note)
+			log("Attach notes migration 0.4.8_2: replaced display for signpost '"..(note.title or "?").."'")
+		end
+	end
+end
+
+
+log("Attach notes: replaced invalid signpost displays")
+
+
